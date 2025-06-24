1273659e211b5c86ae1ae0a2649752964e5fbb28
Keerthi Reddy <keerthireddy@Keerthis-Laptop.local>
2025-06-24 15:07:32 -0400

    Added basic Streamlit app to file upload and preview

---
+++ app.py
@@ -0,0 +1,30 @@
+# app.py
+
+import streamlit as st
+import pandas as pd
+import openpyxl
+
+
+st.title("Smart KPI Recommender")
+
+# --- File Uploader ---
+uploaded_file = st.file_uploader("Upload a CSV or Excel file", type=['csv', 'xlsx'])
+
+if uploaded_file is not None:
+    # --- Try reading file ---
+    try:
+        if uploaded_file.name.endswith('.csv'):
+            df = pd.read_csv(uploaded_file)
+        else:
+            # For Excel: read first sheet by default
+            df = pd.read_excel(uploaded_file)
+        
+        st.success("✅ File successfully loaded!")
+        st.write("### Data Preview", df.head())
+
+    except Exception as e:
+        st.error(f" Oops! Cannot read file: {e}")
+
+else:
+    st.info("👆 Please upload a file to get started.")
+
